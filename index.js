231fccdbad0895f49f7909844f5bbc873fc74de0
Carolimaa <carolniver12@gmail.com>
2023-11-02 19:06:01 -0300

    adicionei o conteúdo do desafio

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 let nomeDoHeroi = "Ralph"
 let xp = 5000
 let nivel = " "
+
+let mensagemNivel = "";
+  
+  if (heroi.quantidadeXp <= 1000) {
+    mensagemNivel = "Ferro";
+  } else if (heroi.quantidadeXp > 1000 && heroi.quantidadeXp <= 2000) {
+    mensagemNivel = "Bronze";
+  } else if (heroi.quantidadeXp > 2000 && heroi.quantidadeXp <= 5000) {
+    mensagemNivel = "Prata";
+  } else if (heroi.quantidadeXp > 6000 && heroi.quantidadeXp <= 7000) {
+    mensagemNivel = "Ouro";
+  } else if (heroi.quantidadeXp > 7000 && heroi.quantidadeXp <= 8000) {
+    mensagemNivel = "Platina";
+  } else if (heroi.quantidadeXp > 8000 && heroi.quantidadeXp <= 9000) {
+    mensagemNivel = "Ascendente";
+  } else if (heroi.quantidadeXp > 9000 && heroi.quantidadeXp <= 10000) {
+    mensagemNivel = "Imortal";
+  } else if (heroi.quantidadeXp >= 10001) {
+    mensagemNivel = "Radiante";
+  }
+
+  console.log("O Herói de nome " + heroi.nome + " está no nível de " + mensagemNivel);
